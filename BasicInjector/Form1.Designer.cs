a40d5a38f7d8c4565a1d6dc3c61ecbdb7f34d84b
0x41 <0x41@bitwise.systems>
2018-08-21 23:03:58 -0500

    Initial commit

---
+++ BasicInjector/Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace BasicInjector
+{
+    sealed partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AllowDrop = true;
+            this.button1.Location = new System.Drawing.Point(194, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(45, 21);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Inject";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.button1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.AllowDrop = true;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "LoadLibrary",
+            "ThreadHijack",
+            "ManualMap"});
+            this.comboBox1.Location = new System.Drawing.Point(92, 249);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(100, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            this.comboBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.comboBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // button2
+            // 
+            this.button2.AllowDrop = true;
+            this.button2.Location = new System.Drawing.Point(194, 199);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(45, 20);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "DLL";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.button2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.Location = new System.Drawing.Point(12, 199);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(180, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "path to dll";
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.textBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // textBox2
+            // 
+            this.textBox2.AllowDrop = true;
+            this.textBox2.Location = new System.Drawing.Point(60, 11);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(118, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "csgo";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            this.textBox2.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.textBox2.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // label1
+            // 
+            this.label1.AllowDrop = true;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label1.Location = new System.Drawing.Point(9, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Process";
+            this.label1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.label1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AllowDrop = true;
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.checkBox1.Location = new System.Drawing.Point(12, 226);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(100, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "wait for process";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            this.checkBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.checkBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.AllowDrop = true;
+            this.numericUpDown1.Enabled = false;
+            this.numericUpDown1.Location = new System.Drawing.Point(193, 225);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            9999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.numericUpDown1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // label3
+            // 
+            this.label3.AllowDrop = true;
+            this.label3.AutoSize = true;
+            this.label3.Enabled = false;
+            this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label3.Location = new System.Drawing.Point(122, 227);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Delay (ms)";
+            this.label3.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.label3.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // label4
+            // 
+            this.label4.AllowDrop = true;
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label4.Location = new System.Drawing.Point(43, 252);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Method";
+            this.label4.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.label4.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(184, 11);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(54, 20);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Refresh";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.button3.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 33);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(227, 160);
+            this.listBox1.Sorted = true;
+            this.listBox1.TabIndex = 13;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            this.listBox1.DragEnter += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 276);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(74, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Save";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(164, 276);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(74, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Load";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(250, 309);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "0x41\'s Injector";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ BasicInjector/Form1.cs
@@ -0,0 +1,228 @@
+﻿using InjectionLibrary;
+using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Globalization;
+
+namespace BasicInjector
+{
+    public sealed partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            AllowDrop = true;
+            DragDrop += Form1_DragDrop;
+        }
+
+        private int _injectionStyle;
+        private IntPtr _hModule;
+        private Process _process;
+        private bool _injected;
+        private InjectionMethod _injector;
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _injectionStyle = comboBox1.SelectedIndex;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (_injected)
+            {
+                _injector.Unload(_hModule, _process.Id);
+
+                if (_injector.GetLastError() != null)
+                    MessageBox.Show(_injector.GetLastError().ToString());
+
+                comboBox1.Enabled = true;
+                textBox1.Enabled = true;
+                textBox2.Enabled = true;
+                button2.Enabled = true;
+                checkBox1.Enabled = true;
+                numericUpDown1.Enabled = true;
+                _injected = false;
+                button1.Text = @"Inject";
+                return;
+            }
+
+            if (checkBox1.Checked)
+            {
+                while (Process.GetProcessesByName(textBox2.Text).Length == 0)
+                {
+                    Thread.Sleep(500);
+                }
+
+                _process = Process.GetProcessesByName(textBox2.Text)[0];
+            }
+            else
+            {
+                _process = Process.GetProcessesByName(textBox2.Text)[0];
+
+                if (_process.Id == 0 && !checkBox1.Checked)
+                {
+                    MessageBox.Show(@"Process not found.");
+                    return;
+                }
+            }
+
+            Thread.Sleep((int)numericUpDown1.Value);
+
+            _injector = InjectionMethod.Create((InjectionMethodType)_injectionStyle);
+            _hModule = _injector.Inject(textBox1.Text, _process.Id);
+
+            //if no errors, return
+            if (_injector.GetLastError() == null)
+            {
+                comboBox1.Enabled = false;
+                textBox1.Enabled = false;
+                textBox2.Enabled = false;
+                button2.Enabled = false;
+                checkBox1.Enabled = false;
+                numericUpDown1.Enabled = false;
+                _injected = true;
+                button1.Text = @"Eject";
+                return;
+            }
+
+            MessageBox.Show(_injector.GetLastError().ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.SelectedIndex = 0;
+            var directoryInfo = Directory.GetParent(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)).Parent;
+            if (directoryInfo != null)
+                textBox1.Text = $@"{directoryInfo.FullName}\source\";
+
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (!listBox1.Items.Contains(process.ProcessName))
+                {
+                    listBox1.Items.Add(process.ProcessName);
+                }
+            }
+
+            LoadSettings();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var file = new OpenFileDialog();
+            file.InitialDirectory = @"C:\";
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = file.FileName;
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            var file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                textBox1.Text = file.FileName;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = checkBox1.Checked;
+        }
+
+        private void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            var file = ((string[])e.Data.GetData(DataFormats.FileDrop))[0];
+            textBox1.Text = file;
+        }
+
+        private void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var processes = Process.GetProcesses();
+            foreach (var process in processes)
+            {
+                if (!listBox1.Items.Contains(process.ProcessName))
+                {
+                    listBox1.Items.Add(process.ProcessName);
+                }
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = listBox1.SelectedItem.ToString();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            LoadSettings();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            SaveSettings();
+        }
+
+        private void LoadSettings()
+        {
+            if (ConfigurationManager.AppSettings["dllpath"] != "")
+                textBox1.Text = ConfigurationManager.AppSettings["dllpath"];
+            textBox2.Text = ConfigurationManager.AppSettings["process"];
+            checkBox1.Checked = Convert.ToBoolean(Convert.ToInt32(ConfigurationManager.AppSettings["waitforprocess"]));
+            numericUpDown1.Value = Convert.ToInt32(ConfigurationManager.AppSettings["delay"]);
+            comboBox1.SelectedIndex = Convert.ToInt32(ConfigurationManager.AppSettings["method"]);
+        }
+
+        private void SaveSettings()
+        {
+            SetSetting("dllpath", textBox1.Text);
+            SetSetting("process", textBox2.Text);
+            SetSetting("waitforprocess", Convert.ToInt32(checkBox1.Checked).ToString());
+            SetSetting("delay", numericUpDown1.Value.ToString(CultureInfo.InvariantCulture));
+            SetSetting("method", comboBox1.SelectedIndex.ToString(CultureInfo.InvariantCulture));
+        }
+
+        internal static bool SetSetting(string key, string value)
+        {
+            var config =
+                ConfigurationManager.OpenExeConfiguration(
+                    ConfigurationUserLevel.None);
+
+            config.AppSettings.Settings.Remove(key);
+            var kvElem = new KeyValueConfigurationElement(key, value);
+            config.AppSettings.Settings.Add(kvElem);
+
+            // Save the configuration file.
+            config.Save(ConfigurationSaveMode.Modified);
+
+            // Force a reload of a changed section.
+            ConfigurationManager.RefreshSection("appSettings");
+
+            return true;
+        }
+    }
+}
